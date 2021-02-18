86b31258b121bd0e81795155663d406c19d63420
LiuLinhong <liulinhong-b@rongcloud.cn>
2021-02-18 15:27:44 +0800

    update rtclib 5.0

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # RongRTCFilterDemo
 
-1. 请登录官网获取AppKey, 并使用自定义userID获取token后, 在ViewController.m中填写如下内容:
+1. 请登录官网获取 AppKey, 并使用自定义 userID 获取 token 后, 在 `ViewController.m` 中填写如下内容:
+   
+    ```objective-c
     self.appKey = @""; //请登录融云官网获取
     self.token = @""; //请使用自定义的userID获取此token
-2. 使用demo中的podfile, 通过 pod install 获取最新版本的融云SDK
-3. pod安装完成后, 请打开pod生成的 RongRTCFilterDemo.xcworkspace 
+    ```
+    
+2. 请在 `pod install` 之前, 使用 `pod repo update` 更新本地资源
+
+3. 使用 demo 中的 podfile , 通过 `pod install` 获取最新版本的融云SDK
+
+4. pod 安装完成后, 请打开 pod 生成的 `RongRTCFilterDemo.xcworkspace`
+
+5. 自定义美颜处理在 `ChatGPUImageHandler.m` 中
--- RongRTCFilterDemo/ViewController.m
+++ RongRTCFilterDemo/ViewController.m
@@ -11,10 +11,11 @@
 #import <RongRTCLib/RongRTCLib.h>
 #import "ChatGPUImageHandler.h"
 
-@interface ViewController () <RongRTCRoomDelegate>
+
+@interface ViewController () <RCRTCRoomEventDelegate>
 
 @property (nonatomic, strong) NSString *appKey, *token;
-@property (nonatomic, strong) RongRTCRoom *room;
+@property (nonatomic, strong) RCRTCRoom *room;
 @property (nonatomic, strong) ChatGPUImageHandler *chatGPUImageHandler;
 
 @end
@@ -31,17 +32,16 @@
     self.appKey = @""; //请登录融云官网获取
     self.token = @""; //请使用自定义的userID获取此token
     NSAssert(self.appKey.length > 0 && self.token > 0, @"请登录融云官网开发者账号获取Appkey, 请使用自定义的userID获取token!");
+    
     [[RCIMClient sharedRCIMClient] initWithAppKey:self.appKey];
     [[RCIMClient sharedRCIMClient] connectWithToken:self.token
+                                           dbOpened:^(RCDBErrorCode code) {}
                                             success:^(NSString *userId) {
         NSLog(@"MClient connectWithToken Success userId: %@", userId);
         [self joinRoom];
     }
                                               error:^(RCConnectErrorCode status) {
         NSLog(@"MClient connectWithToken Error: %zd", status);
-    }
-                                     tokenIncorrect:^{
-        NSLog(@"MClient connectWithToken tokenIncorrect: ");
     }];
 }
 
@@ -49,33 +49,39 @@
 {
     // 设置本地渲染视图
     dispatch_async(dispatch_get_main_queue(), ^{
-        RongRTCLocalVideoView *localView = [[RongRTCLocalVideoView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/2)];
-        localView.fillMode = RCVideoFillModeAspect;
-        [[RongRTCAVCapturer sharedInstance] setVideoRender:localView];
+        RCRTCLocalVideoView *localView = [[RCRTCLocalVideoView alloc] initWithFrame:CGRectMake(0,
+                                                                                               0,
+                                                                                               [UIScreen mainScreen].bounds.size.width,
+                                                                                               [UIScreen mainScreen].bounds.size.height/2)];
+        localView.fillMode = RCRTCVideoFillModeAspect;
+        [[RCRTCEngine sharedInstance].defaultVideoStream setVideoView:localView];
         [self.view addSubview:localView];
     });
     
     // 加入房间
-    [[RongRTCEngine sharedEngine] joinRoom:@"85730573" completion:^(RongRTCRoom * _Nullable room, RongRTCCode code) {
+    [[RCRTCEngine sharedInstance] joinRoom:@"85730573"
+                                completion:^(RCRTCRoom * _Nullable room, RCRTCCode code) {
         room.delegate = self;
         self.room = room;
-        if (code == RongRTCCodeSuccess) {
+        
+        if (code == RCRTCCodeSuccess) {
             //打开摄像头
-            [[RongRTCAVCapturer sharedInstance] startCapture];
+            [[RCRTCEngine sharedInstance].defaultVideoStream startCapture];
             // 发布资源
-            [self.room publishDefaultAVStream:^(BOOL isSuccess,RongRTCCode desc) {
+            [self.room.localUser publishDefaultStreams:^(BOOL isSuccess, RCRTCCode code) {
                 if (isSuccess) {
                     NSLog(@"publishDefaultAVStream Success");
                 } else {
                     NSLog(@"publishDefaultAVStream Failed code: %zd", code);
                 }
             }];
+            
             [self subscribeRemoteUser];
         }
     }];
     
-    [RongRTCAVCapturer sharedInstance].videoSendBufferCallback = ^CMSampleBufferRef _Nullable(BOOL valid, CMSampleBufferRef  _Nullable sampleBuffer) {
-        CMSampleBufferRef processedSampleBuffer = [self.chatGPUImageHandler onGPUFilterSource:sampleBuffer]; //美颜处理
+    [RCRTCEngine sharedInstance].defaultVideoStream.videoSendBufferCallback = ^CMSampleBufferRef _Nullable(BOOL valid, CMSampleBufferRef  _Nullable sampleBuffer) {
+        CMSampleBufferRef processedSampleBuffer = [self.chatGPUImageHandler onGPUFilterSource:sampleBuffer]; //自定义美颜处理
         return processedSampleBuffer;
     };
 }
@@ -87,35 +93,40 @@
     }
     
     NSMutableArray *streams = [NSMutableArray array];
-    for (RongRTCRemoteUser *user in self.room.remoteUsers) {
-        for (RongRTCAVInputStream *stream in user.remoteAVStreams) {
+    for (RCRTCRemoteUser *user in self.room.remoteUsers) {
+        for (RCRTCInputStream *stream in user.remoteStreams) {
             [streams addObject:stream];
         }
     }
     
     NSMutableArray *subscribes = [NSMutableArray new];
-    for (RongRTCAVInputStream *stream in streams) {
+    for (RCRTCInputStream *stream in streams) {
         [subscribes addObject:stream];
-        if (stream.streamType == RTCMediaTypeVideo) {
-            RongRTCRemoteVideoView *videoView = [[RongRTCRemoteVideoView alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height/2, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/2)];
-            videoView.fillMode = RCVideoFillModeAspect;
-            [stream setVideoRender:videoView];
+        if (stream.mediaType == RTCMediaTypeVideo) {
+            RCRTCRemoteVideoView *videoView = [[RCRTCRemoteVideoView alloc] initWithFrame:CGRectMake(0,
+                                                                                                     [UIScreen mainScreen].bounds.size.height / 2,
+                                                                                                     [UIScreen mainScreen].bounds.size.width,
+                                                                                                     [UIScreen mainScreen].bounds.size.height / 2)];
+            videoView.fillMode = RCRTCVideoFillModeAspect;
+            
+            if ([stream isKindOfClass:[RCRTCVideoInputStream class]]) {
+                RCRTCVideoInputStream *tmpVideoInputStream = (RCRTCVideoInputStream *)stream;
+                [tmpVideoInputStream setVideoView:videoView];
+            }
+            
             [self.view addSubview:videoView];
         }
     }
     
     if (subscribes.count > 0) {
         FwLogI(RC_Type_RTC,@"A-appSubscribeStream-T",@"%@all subscribe streams",@"sealRTCApp:");
-        [self.room subscribeAVStream:subscribes tinyStreams:nil completion:^(BOOL isSuccess, RongRTCCode desc) {
-            for (RongRTCAVInputStream *inStream in subscribes) {
-                if (inStream.streamType != RTCMediaTypeVideo) {
-                    continue;
-                }
-                if (isSuccess) {
-                    NSLog(@"subscribeAVStream Success");
-                } else {
-                    NSLog(@"subscribeAVStream Failed, Desc: %@", @(desc));
-                }
+        [self.room.localUser subscribeStream:subscribes
+                                 tinyStreams:nil
+                                  completion:^(BOOL isSuccess, RCRTCCode code) {
+            if (isSuccess) {
+                NSLog(@"subscribeAVStream Success");
+            } else {
+                NSLog(@"subscribeAVStream Failed, Desc: %@", @(code));
             }
         }];
     }
@@ -123,20 +134,31 @@
 
 #pragma mark - RongRTCRoomDelegate
 // 监听发布资源消息
-- (void)didPublishStreams:(NSArray<RongRTCAVInputStream *> *)streams{
+- (void)didPublishStreams:(NSArray <RCRTCInputStream *> *)streams {
     // 设置远端渲染视图
-    for (RongRTCAVInputStream * stream in streams) {
-        if (stream.streamType == RTCMediaTypeVideo) {
+    for (RCRTCInputStream *stream in streams) {
+        if (stream.mediaType == RTCMediaTypeVideo) {
             dispatch_async(dispatch_get_main_queue(), ^{
-                RongRTCRemoteVideoView *videoView = [[RongRTCRemoteVideoView alloc]initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height/2, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height/2)];
-                [stream setVideoRender:videoView];
+                RCRTCRemoteVideoView *videoView = [[RCRTCRemoteVideoView alloc]initWithFrame:CGRectMake(0,
+                                                                                                        [UIScreen mainScreen].bounds.size.height / 2,
+                                                                                                        [UIScreen mainScreen].bounds.size.width,
+                                                                                                        [UIScreen mainScreen].bounds.size.height/2)];
+                videoView.fillMode = RCRTCVideoFillModeAspect;
+                
+                if ([stream isKindOfClass:[RCRTCVideoInputStream class]]) {
+                    RCRTCVideoInputStream *tmpVideoInputStream = (RCRTCVideoInputStream *)stream;
+                    [tmpVideoInputStream setVideoView:videoView];
+                }
+                
                 [self.view addSubview:videoView];
             });
         }
     }
 
     // 订阅资源
-    [self.room subscribeAVStream:streams tinyStreams:nil completion:^(BOOL isSuccess, RongRTCCode desc) {
+    [self.room.localUser subscribeStream:streams
+                             tinyStreams:nil
+                              completion:^(BOOL isSuccess, RCRTCCode code) {
     }];
 }
 
